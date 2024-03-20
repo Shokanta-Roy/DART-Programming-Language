6e88c33c1e4600157f7b6b73813f22ac51f47b62
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-03-21 03:19:49 +0600

    Add files via upload

---
+++ DART-DEMO/01_hello_world.dart
@@ -0,0 +1,15 @@
+void main(){
+  print("hello World");  //this a comment;
+  print("This is my First application");  //this is another comment;
+
+  //Arithmetic operation
+  print(3+3);
+  print(3-3);
+  print(3*3);
+  print(3/3);
+  print(3%3);
+
+  // bool value.
+  print(false);
+  print(true);
+}
---
+++ DART-DEMO/02_Built_in_data_types.dart
@@ -0,0 +1,27 @@
+void main()
+{
+  //numbers (int and double);
+  int x=5;
+  var count=23; // depends on the value.
+
+  double y=10.5;
+  var z=10.10;
+
+  //String
+  String name="Shokanta Roy";
+  var companyName="United International University";
+
+  //Boolean
+  bool isValid=true;
+  var isAlive=false;
+
+  print(x);
+  print(count);
+  print(y);
+  print(z);
+  print(name);
+  print(companyName);
+  print(isValid);
+  print(isAlive);
+
+}
