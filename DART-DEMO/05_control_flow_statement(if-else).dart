46ab9156240d36e0c65eb0fb74f6e51ca2f6b7c8
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-06-02 02:14:41 +0600

    Add files via upload

--- DART-DEMO/05_control_flow_statement(if-else).dart
+++ DART-DEMO/05_control_flow_statement(if-else).dart
@@ -1,9 +1,37 @@
 void main()
 {
   // use of if-else
-  var salary=25000;
-  if(salary>2000){
+  var salary=15000;
+  if(salary>20000){
     print("You got promotion. Congratulation");
   }
+  else{
+    print("You need to work hard");
+  }
+
+  //if-elseif ladder statement
+  var marks=-10;
+  if(marks>= 90 && marks<100){
+    print("GADE A+");
+  }
+  else if(marks >=80 && marks<90){
+    print("GRADE A");
+  }
+  else if(marks >=70 && marks<80){
+    print("GRADE B");
+  }
+  else if(marks >=60 && marks<70){
+    print("GRADE C");
+  }
+  else if(marks >30 && marks<60){
+    print("GRADE D");
+  }
+  else if(marks>=0 && marks<30){
+    print("GRADE F");
+  }
+  else{
+    print("Ivalid marks. Please try again");
+  }
+
 
 }
---
+++ DART-DEMO/08_for_loop.dart
@@ -0,0 +1,21 @@
+void main()
+{
+  //FOR LOOP
+  //WAP to find EVEN number;
+  for(int i=1; i<10; i++)
+    {
+      if(i%2==0)
+        {
+          print(i);
+        }
+        
+
+    }
+
+  List planetList= ["Mercury","Venus", "Earth", "Mars"];
+
+  for(String planet in planetList)
+    {
+      print(planet);
+    }
+}
